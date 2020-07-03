2e8d6ed023324fefae57c6d16dd8e53d2915b847
Rodrigo Reis <rodrigoreis22@yahoo.com.br>
2020-07-03 14:10:14 -0700

    adding secret to git

---
+++ config/prod.secret.exs
@@ -0,0 +1,8 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :chat, Chat.Endpoint,
+  ssl: true
+  secret_key_base: "XR7e8rPXq2nIdBXqtPsyxPz1R1UF3w4HDBFGdxZ+9GDZCT6PpG4aJLpOzehOJVO5"
