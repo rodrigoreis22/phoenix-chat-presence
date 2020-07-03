a8670fed6d13132f10f4ed4d52a6cf04187de125
Rodrigo Reis <rodrigoreis22@yahoo.com.br>
2020-07-03 14:11:15 -0700

    fix typo

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,5 +4,5 @@ use Mix.Config
 # you likely want to automate and keep it away from
 # your version control system.
 config :chat, Chat.Endpoint,
-  ssl: true
+  ssl: true,
   secret_key_base: "XR7e8rPXq2nIdBXqtPsyxPz1R1UF3w4HDBFGdxZ+9GDZCT6PpG4aJLpOzehOJVO5"
